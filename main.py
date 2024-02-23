95c75cb3f499b62cf8a261a6c7ae974aa274c1f6
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-23 14:56:50 +0500

    ad some changes

--- main.py
+++ main.py
@@ -41,13 +41,12 @@ async def get(request: Request):
     return templates.TemplateResponse('main-page.html', {'request': request})
 
 
-@app.websocket("/ws/{client_id}/")
+@app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
-
             await manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
