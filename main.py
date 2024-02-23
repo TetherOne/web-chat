e72d774062f8d75d1a296a544df3e89765f5f075
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-23 21:54:35 +0500

    ad updates

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from fastapi.templating import Jinja2Templates
-
-from fastapi import WebSocketDisconnect
-from fastapi import WebSocket
-from fastapi import FastAPI
-from fastapi import Request
-
 import uvicorn
-
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
@@ -16,20 +11,20 @@ templates = Jinja2Templates(directory='templates')
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket, user_id: str):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[user_id] = websocket
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect(self, user_id: str):
+        del self.active_connections[user_id]
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    async def send_personal_message(self, message: str, user_id: str):
+        await self.active_connections[user_id].send_text(message)
 
     async def broadcast(self, message: str):
-        for connection in self.active_connections:
+        for connection in self.active_connections.values():
             await connection.send_text(message)
 
 
@@ -41,16 +36,16 @@ async def get(request: Request):
     return templates.TemplateResponse('main-page.html', {'request': request})
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
+@app.websocket("/ws/{user_id}")
+async def websocket_endpoint(websocket: WebSocket, user_id: str):
+    await manager.connect(websocket, user_id)
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(f"{user_id}: {data}")
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} has left the chat")
+        manager.disconnect(user_id)
+        await manager.broadcast(f"{user_id} left the chat")
 
 
 # if __name__ == "__main__":
